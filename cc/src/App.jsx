2f40c1e66dca97373c5fcb034b4c220f74980229
Abhishek <103208789+abhishekdotapp@users.noreply.github.com>
2024-09-29 12:38:29 +0000

    Pending changes exported from your codespace

---
+++ cc/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+       
+          <h1 className="bg-indigo-500 font-bold text-3xl text-yellow-300 w-full h-screen" >hello world</h1>
+       
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
